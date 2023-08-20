a2d43e3eef5e914e7d70a99d246f81a29cb2ae93
Rishat <Rshiriev@it-one.ru>
2023-08-20 20:30:52 +0500

    lesson20 получение аргументов запроса @RequestParam

--- src/main/java/org/example/controllers/FirstController.java
+++ src/main/java/org/example/controllers/FirstController.java
@@ -3,18 +3,31 @@ package org.example.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(){
+    public String helloPage(HttpServletRequest request){
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+
+        System.out.printf("Hello, %s %s%n", name, surname);
+
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
-    public String goodByePage(){
+    public String goodByePage(
+            @RequestParam("name") String name,
+            @RequestParam("surname") String surname
+    ){
+        System.out.printf("Hello, %s %s%n", name, surname);
+
         return "first/goodbye";
     }
 }
